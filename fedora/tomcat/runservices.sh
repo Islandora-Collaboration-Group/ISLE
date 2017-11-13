de6a5ddc53bceb3107486d7ac9453f2d6a61dab5
gavincmi <501554+g7morris@users.noreply.github.com>
2017-11-13 13:54:54 -0500

    Official image usage for solr and fedora containers.

--- fedora/tomcat/runservices.sh
+++ fedora/tomcat/runservices.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-service tomcat7 start
+apachectl start
 
-service apache2 start
-
-#tail -F /var/lib/tomcat7/logs/catalina.out
-
-#service tomcat7 restart
-
-#Override the exit command to prevent accidental container distruction
-echo 'alias exit="echo Are you sure? this will kill the container. use Ctrl + p, Ctrl + q to detach or ctrl + d to exit"' > ~/.bashrc
+/bin/bash /usr/local/tomcat/bin/catalina.sh run
 
 #Run bash to keep container running and provide interactive mode
 bash
