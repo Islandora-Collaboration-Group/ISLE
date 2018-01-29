70dd48ed33bbec4c839f125e6c0608d21a33eefd
g7morris <501554+g7morris@users.noreply.github.com>
2018-01-29 12:30:17 -0500

    Updated production template in config. Changed title to isle-prod-project.institution.

--- config/isle-prod-project.institution/mysql/production_site_db.sql
+++ config/isle-prod-project.institution/mysql/production_site_db.sql
@@ -1,4 +1,3 @@
 CREATE DATABASE enduser_institution_drupal_site_db CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER 'enduser_institution_drupal_site_db_user'@'%' IDENTIFIED BY 'enduser_institution_drupal_site_db_pw';
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, ALTER ROUTINE, CREATE ROUTINE, CREATE TEMPORARY TABLES, CREATE VIEW, EVENT, EXECUTE, LOCK TABLES, REFERENCES, SHOW VIEW, TRIGGER ON enduser_institution_drupal_site_db.* TO 'enduser_institution_drupal_site_db_user'@'%';
 FLUSH PRIVILEGES;
