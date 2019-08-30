b1e7a83ed2f01b163b3018ce188b982e4cd95fc5
David Keiser-Clark <dwk2@williams.edu>
2019-08-30 18:02:28 -0400

    bug fix for Issue #312 windows certs

--- scripts/proxy/ssl-certs/local-windows-only.sh
+++ scripts/proxy/ssl-certs/local-windows-only.sh
@@ -6,4 +6,4 @@
 # Optional: You may change the organization ("O=") name (e.g. O=Your Organization)
 
 openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout ../../../config/proxy/ssl-certs/.localdomain.key -out ../../../config/proxy/ssl-certs/.localdomain.pem \
-	-subj "/C=US/O=Islandora Collaboration Group/OU=Domain Control Validated/CN=.localdomain"
+	-subj "//C=US\O=Islandora Collaboration Group\OU=Domain Control Validated\CN=.localdomain"
