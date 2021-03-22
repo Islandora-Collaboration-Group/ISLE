cf93c0176cde06c383cf5ce3bafcebaceaf3f3b0
Gavin Morris <gavin@born-digital.com>
2021-03-22 14:41:19 -0400

    Added new db ut8f migration script - https://github.com/Islandora-Collaboration-Group/ISLE/issues/189

---
+++ scripts/apache/db_ut8f_migration.sh
@@ -0,0 +1,27 @@
+# utf 8 database conversion
+# https://www.drupal.org/project/utf8mb4_convert
+#! /bin/bash
+
+echo "Moving to /var/www/html"
+cd /var/www/html/ || exit;
+
+echo "Download the Drupal module"
+drush @none dl utf8mb4_convert-7.x.10 --pm-force
+
+echo "Clear drush cache"
+drush cc drush
+
+echo "Put Drupal site into Maintenance Mode"
+drush vset maintenance_mode 1
+
+echo "Convert databases to utf8. Please note this process can take 15-30 mins depending."
+drush utf8mb4-convert-databases
+
+echo "Clear Drupal site caches"
+drush cc all
+
+echo "Take Drupal site out of Maintenance Mode"
+drush vset maintenance_mode 0
+
+echo "Script complete"
+exit
